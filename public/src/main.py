4d47da09858c02a64d6f573fc0853b2368c7cc55
BHoran1999 <bhbrianhoran14@gmail.com>
2026-02-24 12:51:11 +0000

    Static Page Established

--- public/src/main.py
+++ public/src/main.py
@@ -1,13 +1,20 @@
 from copy_dir import copy_directory
 from textnode import TextNode
 from generate_pages import generate_pages, generate_pages_recursive 
+import sys
 
 def main():
 
-    #copy_directory("static", "public")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    
+    else:
+        basepath = "/"
 
-    #generate_pages("content/index.md", "template.html", "public/index.html")
+    copy_directory("static", "docs")
 
-    generate_pages_recursive ("content", "template.html", "public")
+    generate_pages("content/index.md", "template.html", "docs/index.html", basepath)
+
+    generate_pages_recursive ("content", "template.html", "docs", basepath)
 
 main()
--- public/src/text_node_to_html_node.py
+++ public/src/text_node_to_html_node.py
@@ -18,10 +18,7 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.LINK:
         node = LeafNode("a", text_node.text, {"href": text_node.url})
         html = node.to_html()
-        print(f"DEBUG LINK: text='{text_node.text}', url='{text_node.url}'")
-        print(f"DEBUG LINK: props={node.props}")
-        print(f"DEBUG LINK: props_to_html()='{node.props_to_html()}'")
-        print(f"DEBUG LINK: Generated HTML: '{html}'")
+
         return node
     if text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
